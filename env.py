af10de9addc6ea8d10608264141867241dbfda96
ethan <ethanowicks9@gmail.com>
2024-02-08 13:39:23 +0000

    Connect elephantsql database and delete sqlite

--- env.py
+++ env.py
@@ -1,2 +1,3 @@
 import os
 os.environ.setdefault("API-KEY", "LCX9HG6HVFNSR6YAACVCVSKJS1S7HHRD1Y99HC8YEJJA0JJSBJGBC8NA2UEZHH1S7PFN0CFNY0T2LCDO")
+os.environ.setdefault("DATABASE_URL", "postgres://bdstpcmg:ICndEvVIqUNouK7gj1SddU4_tnbscxXo@surus.db.elephantsql.com/bdstpcmg")
