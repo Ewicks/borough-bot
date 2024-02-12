155bf3919faed7099711e402a91fbaf5f3e0e377
ethan <ethanowicks9@gmail.com>
2024-02-12 21:23:49 +0000

    Add variable to env file

--- env.py
+++ env.py
@@ -2,3 +2,4 @@ import os
 os.environ.setdefault("API-KEY", "LCX9HG6HVFNSR6YAACVCVSKJS1S7HHRD1Y99HC8YEJJA0JJSBJGBC8NA2UEZHH1S7PFN0CFNY0T2LCDO")
 os.environ.setdefault("DATABASE_URL", "postgres://bdstpcmg:ICndEvVIqUNouK7gj1SddU4_tnbscxXo@surus.db.elephantsql.com/bdstpcmg")
 os.environ.setdefault("SECRET_KEY", "vyfkzcdq!3&k6v&8g($#q1q%=2vp-u!t@w49-a%!dljr6_e^+9")
+os.environ.setdefault("GOOGLE_CHROME_BIN", "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome")
--- websites/bots/richmond_bot.py
+++ websites/bots/richmond_bot.py
@@ -9,6 +9,7 @@ from selenium.webdriver.support.ui import Select
 import re
 from selenium.webdriver import DesiredCapabilities
 from selenium.webdriver.chrome.options import Options
+import os
 
 
 def richmond_bot(startdate, enddate, wordlist):
@@ -48,8 +49,18 @@ def richmond_bot(startdate, enddate, wordlist):
 
     # Set up the WebDriver (you may need to provide the path to your chromedriver executable)
     chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument('headless')
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    
+    # Set Chrome binary location from environment variable
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+
+    # Initialize WebDriver
     driver = webdriver.Chrome(options=chrome_options)
+    # chrome_options = webdriver.ChromeOptions()
+    # chrome_options.add_argument('headless')
+    # driver = webdriver.Chrome(options=chrome_options)
 
 
     url = 'https://www2.richmond.gov.uk/lbrplanning/Planning_Report.aspx'
